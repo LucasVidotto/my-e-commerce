f4664edd05d0eaa17b41192afbd3d01d3c8854df
Lucas Vidotto <lucasvidotto3@gmail.com>
2022-09-18 22:39:57 -0300

    controle de itens estatico

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 import './home.css';
 
 export function Home() {
+    const [quanti, setQuanti] = useState(10);
+    const [mensagem, setMensagem] = useState('faltando itens');
+    function handlerClick(number) {
+
+        if (number === 1) {
+            /* if (quanti <= 0) {
+                setMensagem('sem produtos no estoque');
+            } else */
+            setQuanti(quanti - 1);
+        } else if (number === 2) {
+            setQuanti(quanti + 1);
+        }
+
+    }
     return (
         <div className="container-home">
             <div className='section-home'>
@@ -11,11 +25,20 @@ export function Home() {
                 </div>
                 <div className="container-image">
                     <img src="https://wallpaper.dog/large/20509487.jpg" alt="" />
-                    <span>Descrição do item</span>
+                    <span>Descrição do item </span>
                 </div>
                 <div className="container-image">
                     <img src="https://wallpaper.dog/large/20509487.jpg" alt="" />
-                    <span>Descrição do item</span>
+                    <div className='span-center'>
+                        <span className='span-home'>Descrição do item</span>
+                        <span>Quantidade : {quanti}</span>
+                        {quanti <= 0 ? <span>{mensagem}</span> : null}
+
+                    </div>
+                    <div className='btn-class'>
+                        <button name="more" className="btn-home" type="button" onClick={() => handlerClick(2)}> + </button>
+                        <button name="less" className="btn-home" type="button" onClick={() => handlerClick(1)}> - </button>
+                    </div>
                 </div>
 
             </div>
@@ -50,6 +73,6 @@ export function Home() {
                 </div>
 
             </div>
-        </div>
+        </div >
     )
 }
